98a8f83e598d42a8cf94672e628ccf0473575fda
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-05 19:42:39 -0300

    <store> configurando state

---
+++ src/store/state.js
@@ -0,0 +1,20 @@
+const state = {
+  productItems: [],
+  cartItems: {
+    internet: {
+      product: '',
+      price: 0,
+    },
+    tv: {
+      product: '',
+      price: 0,
+    },
+    fixo: {
+      product: '',
+      price: 0,
+    },
+  },
+  totalPrice: 0,
+};
+
+export default state;
