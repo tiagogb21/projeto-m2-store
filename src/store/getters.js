6d2674f56b0b31bc61b7ddc0fdeeb247e0661c81
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-05 19:42:11 -0300

    <store> configurando getters

---
+++ src/store/getters.js
@@ -0,0 +1,42 @@
+const getters = {
+  productItems: (state) => () => state.productItems,
+  // Acessando o nome das categorias:
+
+  productList: (state) => () => Object.keys(state.productItems),
+  // Acessando o valor armazenado no objeto fixo:
+
+  productFixo: (state) => () => Object.values(state.productItems)[0][0],
+
+  productTV: (state) => () => Object.values(state.productItems)[1].map((item) => item),
+
+  productInternet: (state) => () => Object.values(state.productItems)[2].map((item) => item),
+
+  addProductToCart: (state) => (name, prod) => {
+    state.cartItems[name].product = prod.title;
+    state.cartItems[name].price = prod.price;
+    const sumTotalPrice = Object.values(state.cartItems).reduce((acc, curr) => acc + curr.price);
+    state.totalPrice = sumTotalPrice;
+    return state.totalPrice;
+  },
+
+  removeAllItems: (state) => () => {
+    state.totalPrice = 0;
+    const resetCart = {
+      internet: {
+        product: '',
+        price: 0,
+      },
+      tv: {
+        product: '',
+        price: 0,
+      },
+      fixo: {
+        product: '',
+        price: 0,
+      },
+    };
+    state.cartItems = resetCart;
+  },
+};
+
+export default getters;
