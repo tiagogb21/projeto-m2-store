2a6282d52da641b6e4b0adf65b059dae9da303be
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-05 19:50:37 -0300

    <header><footer> estruturando header e footer

---
+++ src/components/footer/FooterPrincipal.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class>
+    <h3>Todos os direitos reservados</h3>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterPrincipal',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  margin-top: 3vw;
+  width: 100vw;
+  background: black;
+  color: white;
+  text-align: center;
+}
+</style>
---
+++ src/components/header/HeaderPrincipal.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header__container">
+    <h1>M2-Store</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPrincipal',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+header {
+  width: 100vw;
+  background: rgb(4, 4, 90);
+  color: rgb(255, 255, 255);
+  text-align: center;
+}
+</style>
